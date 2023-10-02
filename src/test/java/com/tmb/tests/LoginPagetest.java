a77ad97e4f549b8c8a1c782c0219844960ec42d4
Gagan Kaushik <gagan.kaushik@nagarro.com>
2023-10-03 00:15:15 +0530

    project-1

---
+++ src/test/java/com/tmb/tests/LoginPagetest.java
@@ -0,0 +1,41 @@
+package com.tmb.tests;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.tmb.constants.FrameworkConstants;
+
+//import org.testng.annotations.Test;
+
+public class LoginPagetest extends FrameworkConstants {
+	
+	
+	
+	@Test
+	public void test1() throws InterruptedException {
+//		System.setProperty("webdriver.chrome.driver";
+//		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		//driver.findElement(By.name("q"))
+		driver.findElement(By.xpath("//*[@name='q']")).sendKeys("bullet",Keys.ENTER);
+		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3")).click();
+		Thread.sleep(5000);
+		driver.quit();
+	}
+	
+		@Test(priority=2)
+		public void test2() {
+			//System.setProperty("webdriver.chrome.driver",FrameworkConstants.CHROMEDRIVERPATH);
+			//WebDriver driver = new ChromeDriver(); 
+			driver.get("https://www.google.com/");
+
+			driver.findElement(By.name("q")).sendKeys("tvs",Keys.ENTER);
+			driver.quit();
+		}
+}
+ 
