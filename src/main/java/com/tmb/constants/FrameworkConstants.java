220384a8001201f0701092c51bfc41abcab28488
Gagan Kaushik <gagan.kaushik@nagarro.com>
2023-10-03 10:20:43 +0530

    update

--- src/main/java/com/tmb/constants/FrameworkConstants.java
+++ src/main/java/com/tmb/constants/FrameworkConstants.java
@@ -2,15 +2,14 @@ package com.tmb.constants;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.BeforeMethod;
 
 public class FrameworkConstants {
     public static WebDriver driver;
 
-    @BeforeMethod
+    
     public void chrome() {
-        // Set the path to the ChromeDriver executable (uncomment if needed)
-        // System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/executables/chromedriver.exe");
+
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
