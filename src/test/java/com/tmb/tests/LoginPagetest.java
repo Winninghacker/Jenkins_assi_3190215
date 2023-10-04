d79ccada08398cbf5a4807496f6cedb0ea1eee13
Winninghacker <92116290+Winninghacker@users.noreply.github.com>
2023-10-04 21:03:03 +0530

    Update LoginPagetest.java

--- src/test/java/com/tmb/tests/LoginPagetest.java
+++ src/test/java/com/tmb/tests/LoginPagetest.java
@@ -38,50 +38,6 @@ public class LoginPagetest extends FrameworkConstants {
 		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3")).click();
 		Thread.sleep(5000);
 		driver.quit();}
-	@Test(priority=3)
-	public void test3() throws InterruptedException {
-//		System.setProperty("webdriver.chrome.driver";
-//		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.google.com/");
-		//driver.findElement(By.name("q"))
-		driver.findElement(By.xpath("//*[@name='q']")).sendKeys("bullet",Keys.ENTER);
-		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3")).click();
-		Thread.sleep(5000);
-		driver.quit();}
-	@Test(priority=4)
-	public void test4() throws InterruptedException {
-//		System.setProperty("webdriver.chrome.driver";
-//		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.google.com/");
-		//driver.findElement(By.name("q"))
-		driver.findElement(By.xpath("//*[@name='q']")).sendKeys("bullet",Keys.ENTER);
-		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3")).click();
-		Thread.sleep(5000);
-		driver.quit();}
-	@Test(priority=5)
-	public void test5() throws InterruptedException {
-//		System.setProperty("webdriver.chrome.driver";
-//		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.google.com/");
-		//driver.findElement(By.name("q"))
-		driver.findElement(By.xpath("//*[@name='q']")).sendKeys("bullet",Keys.ENTER);
-		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3")).click();
-		Thread.sleep(5000);
-		driver.quit();}
-	@Test(priority=6)
-	public void test6() throws InterruptedException {
-//		System.setProperty("webdriver.chrome.driver";
-//		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.google.com/");
-		//driver.findElement(By.name("q"))
-		driver.findElement(By.xpath("//*[@name='q']")).sendKeys("bullet",Keys.ENTER);
-		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3")).click();
-		Thread.sleep(5000);
-		driver.quit();}
 	
 	
 		
