d383f99193b041f6f1d2dcaa34a9f1f6121132c9
Gagan Kaushik <gagan.kaushik@nagarro.com>
2023-10-04 14:15:19 +0530

    updated test case

--- src/main/java/com/tmb/constants/FrameworkConstants.java
+++ src/main/java/com/tmb/constants/FrameworkConstants.java
@@ -2,12 +2,14 @@ package com.tmb.constants;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 //import org.testng.annotations.BeforeMethod;
 
 public class FrameworkConstants {
+	
     public static WebDriver driver;
 
-    
+    @BeforeMethod
     public void chrome() {
 
 
